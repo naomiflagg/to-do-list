446d4bcc497a59c80fcbd73c4ac08d75e98f07a2
Naomi Flagg <neffly511@gmail.com>
2020-12-07 20:06:25 -0800

    Create checkboxes for tasks

--- src/editDom.js
+++ src/editDom.js
@@ -7,14 +7,23 @@ const editDom = (() => {
     },
 
     addTask(task) {
-      let newTask = document.createElement('li');
       let taskList = document.querySelector('.tasks');
+      let newTask = document.createElement('li');
+      let input = document.createElement('input');
+      let label = document.createElement('label')
       tasks.push(task)
       taskList.appendChild(newTask);
-      newTask.textContent = task.name
-      
+      newTask.appendChild(input);
+      newTask.appendChild(label);
+      let id = tasks.indexOf(task);
+      input.setAttribute('type', 'checkbox');
+      input.setAttribute('id', id)
+      label.setAttribute('for', id)
+      label.textContent = task.name;
     }
   }
 })();
+
+//if i remove tasks, need to update the ids of the index. make sure they're updated
 
 export default editDom
